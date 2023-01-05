9ad1504e380fcd808ebe58c3b2767ffff847f5ad
TCreograph <121938011+TCreograph@users.noreply.github.com>
2023-01-05 10:29:12 +0000

    Deploying to gh-pages from @ TCreograph/TCreograph.github.io@f9eae8730318ce6825819b48799a73cf24854b08 🚀

---
+++ assets/mermaid-mindmap.esm.min-ac8497d0.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app-95e8bea8.js";import"./framework-e7f5dc3a.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2-4dda171c.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};
